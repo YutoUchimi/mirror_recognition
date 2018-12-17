ccb93595cbe4c6df857a8fba150d58b729b29f5c
Yuto Uchimi <momosama1072@gmail.com>
2018-12-17 16:43:45 +0900

    Add infer_fcn8s_mirror_segmentation.py

--- learning_scripts/infer_fcn8s_mirror_segmentation.py
+++ learning_scripts/infer_fcn8s_mirror_segmentation.py
@@ -20,32 +20,17 @@ import numpy as np
 
 sys.path.append(osp.dirname(osp.dirname(osp.abspath(__file__))))
 
-from datasets import LabeledMirrorObjectDataset  # NOQA
-from models import FCN8sMirrorObjectSegmentation  # NOQA
+from datasets import LabeledMirrorDataset  # NOQA
+from models import FCN8sMirrorSegmentation  # NOQA
 
 
-class FCNMirrorObjectSegmentation(object):
+class FCNMirrorSegmentation(object):
 
-    mirror_class_names = np.array([
+    class_names = np.array([
         '_background_',
         'mirror',
     ], dtype=np.str)
-    object_class_names = np.array([
-        '_background_',
-        '_tote_',
-        'band_aid_tape',
-        'bath_sponge',
-        'burts_bees_baby_wipes',
-        'colgate_toothbrush_4pk',
-        'crayons',
-        'duct_tape',
-        'expo_eraser',
-        'hanes_socks',
-        'laugh_out_loud_jokes',
-        'tissue_box',
-    ], dtype=np.str)
-    mirror_class_names.setflags(write=0)
-    object_class_names.setflags(write=0)
+    class_names.setflags(write=0)
 
     def __init__(self):
         parser = argparse.ArgumentParser()
@@ -85,15 +70,15 @@ class FCNMirrorObjectSegmentation(object):
         self.process()
 
     def load_model(self):
-        model_file = osp.join(self.data_dir, 'max_mean_iu_object.npz')
+        model_file = osp.join(self.data_dir, 'max_miou.npz')
         with open(osp.join(self.data_dir, 'model.txt'), 'r') as f:
             model_name = f.readline().rstrip()
         with open(osp.join(self.data_dir, 'n_class.txt'), 'r') as f:
             n_class = int(f.readline().rstrip())
         self.n_class = n_class
 
-        if model_name == 'FCN8sMirrorObjectSegmentation':
-            self.model = FCN8sMirrorObjectSegmentation(n_class_object=n_class)
+        if model_name == 'FCN8sMirrorSegmentation':
+            self.model = FCN8sMirrorSegmentation(n_class=n_class)
 
         print('\nLoading trained model:          {0}'.format(model_file))
         S.load_npz(model_file, self.model)
@@ -108,8 +93,8 @@ class FCNMirrorObjectSegmentation(object):
     def load_dataset(self):
         with open(osp.join(self.data_dir, 'dataset.txt'), 'r') as f:
             dataset_name = f.readline().rstrip()
-        if dataset_name == 'LabeledMirrorObjectDataset':
-            self.dataset = LabeledMirrorObjectDataset(
+        if dataset_name == 'LabeledMirrorDataset':
+            self.dataset = LabeledMirrorDataset(
                 split=self.split, aug=False)
         self.data_len = len(self.dataset)
         print('dataset class name: %s' % self.dataset.__class__.__name__)
@@ -117,7 +102,7 @@ class FCNMirrorObjectSegmentation(object):
         print('dataset length: %d' % self.data_len)
         print('n_class: %d' % self.n_class)
 
-    def _transform(self, rgb_img, gt_mirror, gt_object):
+    def _transform(self, rgb_img, gt):
         # RGB -> BGR
         bgr_img = rgb_img[:, :, ::-1]
         bgr_img = bgr_img.astype(np.float32)
@@ -125,36 +110,28 @@ class FCNMirrorObjectSegmentation(object):
         bgr_img -= mean_bgr
         # H, W, C -> C, H, W
         bgr_img = bgr_img.transpose((2, 0, 1))
-        return bgr_img, gt_mirror, gt_object
+        return bgr_img, gt
 
     def summarizing_process(self):
         print('========================================================')
         print('Summary of segmentation and depth accuracy')
 
-        sum_mean_iu_mirror = 0.0
-        sum_mean_iu_object = 0.0
+        sum_miou = 0.0
         id = 0
         for id in range(self.data_len):
-            rgb_img, gt_mirror, gt_object = self.dataset[id]
-            bgr_img, gt_mirror, gt_object = self._transform(
-                rgb_img, gt_mirror, gt_object)
-            pred_label_mirror, pred_label_object = self._segment(bgr_img)
-            pred_label_mirror = pred_label_mirror.astype(np.int32)
-            pred_label_object = pred_label_object.astype(np.int32)
+            rgb_img, gt = self.dataset[id]
+            bgr_img, gt = self._transform(rgb_img, gt)
+            pred_label = self._segment(bgr_img)
+            pred_label = pred_label.astype(np.int32)
 
             # evaluate mean IU
-            mean_iu_mirror = fcn.utils.label_accuracy_score(
-                [gt_mirror], [pred_label_mirror], n_class=2)[2]
-            mean_iu_object = fcn.utils.label_accuracy_score(
-                [gt_object], [pred_label_object], n_class=self.n_class)[2]
-            sum_mean_iu_mirror += mean_iu_mirror
-            sum_mean_iu_object += mean_iu_object
+            miou = fcn.utils.label_accuracy_score(
+                [gt], [pred_label], n_class=2)[2]
+            sum_miou += miou
 
-        ave_mean_iu_mirror = sum_mean_iu_mirror / self.data_len
-        ave_mean_iu_object = sum_mean_iu_object / self.data_len
+        ave_miou = sum_miou / self.data_len
 
-        print('mean IU (mirror): %lf' % ave_mean_iu_mirror)
-        print('mean IU (object): %lf' % ave_mean_iu_object)
+        print('mean IU: %lf' % ave_miou)
         print('========================================================\n')
 
     def process(self):
@@ -163,41 +140,27 @@ class FCNMirrorObjectSegmentation(object):
             print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
             print('id: %d' % id)
 
-            rgb_img, gt_mirror, gt_object = self.dataset[id]
-            bgr_img, gt_mirror, gt_object = self._transform(
-                rgb_img, gt_mirror, gt_object)
-            pred_label_mirror, pred_label_object = self._segment(bgr_img)
-            pred_label_mirror = pred_label_mirror.astype(np.int32)
-            pred_label_object = pred_label_object.astype(np.int32)
+            rgb_img, gt = self.dataset[id]
+            bgr_img, gt = self._transform(rgb_img, gt)
+            pred_label = self._segment(bgr_img)
+            pred_label = pred_label.astype(np.int32)
 
             # evaluate mean IU
-            mean_iu_mirror = fcn.utils.label_accuracy_score(
-                [gt_mirror], [pred_label_mirror], n_class=2)[2]
-            mean_iu_object = fcn.utils.label_accuracy_score(
-                [gt_object], [pred_label_object], n_class=self.n_class)[2]
-            print('mean IU (mirror): %lf' % mean_iu_mirror)
-            print('mean IU (object): %lf' % mean_iu_object)
+            miou = fcn.utils.label_accuracy_score(
+                [gt], [pred_label], n_class=2)[2]
+            print('mean IU: %lf' % miou)
 
             # visualize label, proba
-            gt_mirror_viz = mvtk.image.label2rgb(
-                gt_mirror, img=rgb_img,
-                label_names=self.mirror_class_names, alpha=0.7)
-            gt_object_viz = mvtk.image.label2rgb(
-                gt_object, img=rgb_img,
-                label_names=self.object_class_names, alpha=0.7)
-            pred_label_mirror_viz = mvtk.image.label2rgb(
-                pred_label_mirror, img=rgb_img,
-                label_names=self.mirror_class_names, alpha=0.7)
-            pred_label_object_viz = mvtk.image.label2rgb(
-                pred_label_object, img=rgb_img,
-                label_names=self.object_class_names, alpha=0.7)
+            gt_viz = mvtk.image.label2rgb(
+                gt, img=rgb_img,
+                label_names=self.class_names, alpha=0.7)
+            pred_label_viz = mvtk.image.label2rgb(
+                pred_label, img=rgb_img,
+                label_names=self.class_names, alpha=0.7)
 
-            random_image = np.random.randint(
-                0, 256, size=rgb_img.shape, dtype=np.uint8)
             viz = mvtk.image.tile(
-                [rgb_img, pred_label_mirror_viz, pred_label_object_viz,
-                 random_image, gt_mirror_viz, gt_object_viz],
-                shape=(2, 3))
+                [rgb_img, pred_label_viz, gt_viz],
+                shape=(1, 3))
             mvtk.image.resize(viz, size=300 * 300)
             print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
             viz = cv2.resize(viz, None, None, fx=0.7, fy=0.7)
@@ -224,39 +187,29 @@ class FCNMirrorObjectSegmentation(object):
 
         if LooseVersion(chainer.__version__) < LooseVersion('2.0.0'):
             bgr = chainer.Variable(bgr_data, volatile=True)
-            self.model(bgr, None, None)
+            self.model(bgr, None)
         else:
             with chainer.using_config('train', False):
                 with chainer.no_backprop_mode():
                     bgr = chainer.Variable(bgr_data)
-                    self.model(bgr, None, None)
+                    self.model(bgr, None)
 
         # Get proba_img, pred_label
-        proba_img_mirror = F.softmax(self.model.score_mirror)
-        proba_img_mirror = F.transpose(proba_img_mirror, (0, 2, 3, 1))
-        max_proba_img_mirror = F.max(proba_img_mirror, axis=-1)
+        proba_img = F.softmax(self.model.score)
+        proba_img = F.transpose(proba_img, (0, 2, 3, 1))
+        max_proba_img = F.max(proba_img, axis=-1)
 
-        proba_img_object = F.softmax(self.model.score_object)
-        proba_img_object = F.transpose(proba_img_object, (0, 2, 3, 1))
-        max_proba_img_object = F.max(proba_img_object, axis=-1)
-
-        pred_label_mirror = F.argmax(self.model.score_mirror, axis=1)
-        pred_label_object = F.argmax(self.model.score_object, axis=1)
+        pred_label = F.argmax(self.model.score, axis=1)
 
         # squeeze batch axis, gpu -> cpu
-        max_proba_img_mirror = cuda.to_cpu(max_proba_img_mirror.data)[0]
-        max_proba_img_object = cuda.to_cpu(max_proba_img_object.data)[0]
-        pred_label_mirror = cuda.to_cpu(pred_label_mirror.data)[0]
-        pred_label_object = cuda.to_cpu(pred_label_object.data)[0]
+        max_proba_img = cuda.to_cpu(max_proba_img.data)[0]
+        pred_label = cuda.to_cpu(pred_label.data)[0]
 
         # uncertain because the probability is low
-        pred_label_mirror[
-            max_proba_img_mirror < self.proba_threshold] = self.bg_label
-        pred_label_object[
-            max_proba_img_object < self.proba_threshold] = self.bg_label
+        pred_label[max_proba_img < self.proba_threshold] = self.bg_label
 
-        return pred_label_mirror, pred_label_object
+        return pred_label
 
 
 if __name__ == '__main__':
-    FCNMirrorObjectSegmentation()
+    FCNMirrorSegmentation()
