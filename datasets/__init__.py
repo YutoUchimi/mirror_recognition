b681b1577a6cb94e2e2e5dc05a4025b5d2b9aa94
Yuto Uchimi <momosama1072@gmail.com>
2018-12-18 21:02:05 +0900

    Add scripts for LabeledMirrorWithDepthDataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,3 +1,4 @@
 # flake8: NOQA
 from .labeled_mirror import LabeledMirrorDataset
+from .labeled_mirror import LabeledMirrorWithDepthDataset
 from .labeled_mirror_object import LabeledMirrorObjectDataset
---
+++ learning_scripts/view_labeled_mirror_with_depth_dataset.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import argparse
+import os.path as osp
+import sys
+
+import mvtk
+
+sys.path.append(osp.dirname(osp.dirname(osp.abspath(__file__))))
+from datasets import LabeledMirrorWithDepthDataset  # NOQA
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--split', required=True,
+                        choices=['train', 'test'])
+    parser.add_argument('-a', '--aug', type=int, default=0,
+                        help='Enable data augmentation if this is set to 1')
+    args = parser.parse_args()
+
+    dataset = LabeledMirrorWithDepthDataset(args.split, aug=args.aug)
+    mvtk.datasets.view_dataset(
+        dataset, LabeledMirrorWithDepthDataset.visualize)
+
+
+if __name__ == '__main__':
+    main()
