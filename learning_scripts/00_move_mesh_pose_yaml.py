9dd6600e2d087faab20a11c06f2c3c507c8f85ef
Yuto Uchimi <momosama1072@gmail.com>
2019-01-07 22:07:44 +0900

    Add 00_move_mesh_pose_yaml.py

---
+++ learning_scripts/00_move_mesh_pose_yaml.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import argparse
+import glob
+import os
+import os.path as osp
+import shutil
+
+
+def move_mesh_pose_yaml(root_dir):
+    # root_dir must contain timestamp data dirs and only one pose/ dir.
+    # The name of timestamp dirs existing in pose/ dir may be differerent
+    # from that existing in root_dir.
+    root_dir = osp.expanduser(root_dir.rstrip('/'))
+    if not osp.isdir(root_dir):
+        print('[ERROR]: {} is not directory.'.format(root_dir))
+        exit(1)
+
+    src_dir = osp.join(root_dir, 'pose')
+    dst_dir = root_dir
+    if not osp.isdir(src_dir):
+        print('[ERROR]: {} is not directory.'.format(src_dir))
+        exit(1)
+
+    assert len(os.listdir(src_dir)) == len(os.listdir(dst_dir)) - 1
+
+    # Move
+    length = len(os.listdir(src_dir))
+    for i, stamp_dir_ in enumerate(sorted(os.listdir(src_dir))):
+        print('Moving mesh pose: {} / {}'.format(i + 1, length))
+        src_stamp_dir = osp.join(src_dir, stamp_dir_)
+        for file in glob.glob(osp.join(src_stamp_dir, '*')):
+            shutil.copy(
+                file, osp.join(dst_dir, sorted(os.listdir(dst_dir))[i]))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--root_dir', type=str, required=True)
+    args = parser.parse_args()
+    root_dir = args.root_dir
+
+    move_mesh_pose_yaml(root_dir)
