f986784bd47e98834ec5fe6f28c1e568848f99e9
Yuto Uchimi <momosama1072@gmail.com>
2019-01-09 10:43:39 +0900

    Remove no more needed pose/ dir in 00_move_mesh_pose_yaml.py

--- learning_scripts/00_move_mesh_pose_yaml.py
+++ learning_scripts/00_move_mesh_pose_yaml.py
@@ -24,19 +24,23 @@ def move_mesh_pose_yaml(root_dir):
 
     assert len(os.listdir(src_dir)) == len(os.listdir(dst_dir)) - 1
 
-    # Move
+    # Copy
     length = len(os.listdir(src_dir))
     for i, stamp_dir_ in enumerate(sorted(os.listdir(src_dir))):
         print('Moving mesh pose: {} / {}'.format(i + 1, length))
         src_stamp_dir = osp.join(src_dir, stamp_dir_)
-        for file in glob.glob(osp.join(src_stamp_dir, '*')):
-            shutil.copy(
-                file, osp.join(dst_dir, sorted(os.listdir(dst_dir))[i]))
+        for f in glob.glob(osp.join(src_stamp_dir, '*')):
+            shutil.copy(f, osp.join(dst_dir, sorted(os.listdir(dst_dir))[i]))
+
+    # Remove no more needed pose/ dir
+    shutil.rmtree(src_dir)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--root_dir', type=str, required=True)
+    parser.add_argument(
+        '-d', '--root_dir', type=str, required=True,
+        help='Must contain timestamp data dirs and only one pose/ dir.')
     args = parser.parse_args()
     root_dir = args.root_dir
 
