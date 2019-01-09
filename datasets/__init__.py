02f9015d17c38892858d3fba72b2e5c246528440
Yuto Uchimi <momosama1072@gmail.com>
2019-01-09 11:23:04 +0900

    Add Mirror3DAnnotatedDataset class

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,3 +2,4 @@
 from .labeled_mirror import LabeledMirrorDataset
 from .labeled_mirror import LabeledMirrorWithDepthDataset
 from .labeled_mirror_object import LabeledMirrorObjectDataset
+from .mirror_3d_annotated import Mirror3DAnnotatedDataset
