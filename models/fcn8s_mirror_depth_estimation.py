efb338b46eba35cf0fb7b35643a73c4758566325
Yuto Uchimi <momosama1072@gmail.com>
2019-01-09 14:06:31 +0900

    Fix FCN8sMirrorDepthEstimation

--- models/fcn8s_mirror_depth_estimation.py
+++ models/fcn8s_mirror_depth_estimation.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import os.path as osp
 
 import chainer
@@ -10,15 +12,15 @@ import numpy as np
 
 class FCN8sMirrorDepthEstimation(chainer.Chain):
 
-    # 0.2 < depth < 3.0
-    min_depth = 0.2
-    max_depth = 3.0
+    # 0.5 < depth < 5.0
+    min_depth = 0.5
+    max_depth = 5.0
 
     pretrained_model = osp.expanduser(
         '~/data/models/chainer/fcn8s-atonce_from_caffe.npz')
 
-    def __init__(self, n_class_including_mirror):
-        n_class = n_class_including_mirror
+    def __init__(self, n_class):
+        n_class = n_class
         self.n_class = n_class
         kwargs = {
             'initialW': chainer.initializers.Zero(),
@@ -44,47 +46,43 @@ class FCN8sMirrorDepthEstimation(chainer.Chain):
             self.conv5_2 = L.Convolution2D(512, 512, 3, 1, 1, **kwargs)
             self.conv5_3 = L.Convolution2D(512, 512, 3, 1, 1, **kwargs)
 
-            self.fc6 = L.Convolution2D(512, 4096, 7, 1, 0, **kwargs)
-            self.fc7 = L.Convolution2D(4096, 4096, 1, 1, 0, **kwargs)
+            self.fc6_label = L.Convolution2D(512, 4096, 7, 1, 0, **kwargs)
+            self.fc7_label = L.Convolution2D(4096, 4096, 1, 1, 0, **kwargs)
 
             self.score_fr_label = L.Convolution2D(
                 4096, n_class, 1, 1, 0, **kwargs)
-
             self.upscore2_label = L.Deconvolution2D(
                 n_class, n_class, 4, 2, 0, nobias=True,
                 initialW=fcn.initializers.UpsamplingDeconvWeight())
             self.upscore8_label = L.Deconvolution2D(
                 n_class, n_class, 16, 8, 0, nobias=True,
                 initialW=fcn.initializers.UpsamplingDeconvWeight())
-
             self.score_pool3_label = L.Convolution2D(
                 256, n_class, 1, 1, 0, **kwargs)
             self.score_pool4_label = L.Convolution2D(
                 512, n_class, 1, 1, 0, **kwargs)
-
             self.upscore_pool4_label = L.Deconvolution2D(
                 n_class, n_class, 4, 2, 0, nobias=True,
                 initialW=fcn.initializers.UpsamplingDeconvWeight())
 
-            self.score_fr_depth = L.Convolution2D(
-                4096, 2, 1, 1, 0, **kwargs)
+            self.fc6_depth = L.Convolution2D(512, 4096, 7, 1, 0, **kwargs)
+            self.fc7_depth = L.Convolution2D(4096, 4096, 1, 1, 0, **kwargs)
 
+            self.score_fr_depth = L.Convolution2D(4096, 1, 1, 1, 0, **kwargs)
             self.upscore2_depth = L.Deconvolution2D(
-                2, 2, 4, 2, 0, nobias=True,
+                1, 1, 4, 2, 0, nobias=True,
                 initialW=fcn.initializers.UpsamplingDeconvWeight())
             self.upscore8_depth = L.Deconvolution2D(
-                2, 2, 16, 8, 0, nobias=True,
+                1, 1, 16, 8, 0, nobias=True,
                 initialW=fcn.initializers.UpsamplingDeconvWeight())
-
-            self.score_pool3_depth = L.Convolution2D(256, 2, 1, 1, 0, **kwargs)
-            self.score_pool4_depth = L.Convolution2D(512, 2, 1, 1, 0, **kwargs)
-
+            self.score_pool3_depth = L.Convolution2D(256, 1, 1, 1, 0, **kwargs)
+            self.score_pool4_depth = L.Convolution2D(512, 1, 1, 1, 0, **kwargs)
             self.upscore_pool4_depth = L.Deconvolution2D(
-                2, 2, 4, 2, 0, nobias=True,
+                1, 1, 4, 2, 0, nobias=True,
                 initialW=fcn.initializers.UpsamplingDeconvWeight())
 
-    def predict_mask(self, rgb, depth_viz):
-        h = F.concat((rgb, depth_viz), axis=1)
+    def predict_label(self, bgr, depth_bgr):
+        h = F.concat((bgr, depth_bgr), axis=1)
 
         h = F.relu(self.conv1_1(h))
         h = F.relu(self.conv1_2(h))
@@ -114,15 +112,15 @@ class FCN8sMirrorDepthEstimation(chainer.Chain):
         h = F.max_pooling_2d(h, 2, stride=2, pad=0)
         pool5 = h  # 1/32
 
-        h = F.relu(self.fc6(pool5))
+        h = F.relu(self.fc6_label(pool5))
         h = F.dropout(h, ratio=0.5)
-        fc6 = h  # 1/32
+        fc6_label = h  # 1/32
 
-        h = F.relu(self.fc7(fc6))
+        h = F.relu(self.fc7_label(fc6_label))
         h = F.dropout(h, ratio=0.5)
-        fc7 = h  # 1/32
+        fc7_label = h  # 1/32
 
-        h = self.score_fr_label(fc7)
+        h = self.score_fr_label(fc7_label)
         score_fr_label = h  # 1/32
 
         h = 0.01 * pool4
@@ -132,9 +130,11 @@ class FCN8sMirrorDepthEstimation(chainer.Chain):
         h = self.upscore2_label(score_fr_label)
         upscore2_label = h  # 1/16
 
-        h = score_pool4_label[:, :,
-                              5:5 + upscore2_label.data.shape[2],
-                              5:5 + upscore2_label.data.shape[3]]
+        h = score_pool4_label[
+            :, :,
+            5:5 + upscore2_label.data.shape[2],
+            5:5 + upscore2_label.data.shape[3]
+        ]
         score_pool4c_label = h  # 1/16
 
         h = upscore2_label + score_pool4c_label
@@ -147,9 +147,11 @@ class FCN8sMirrorDepthEstimation(chainer.Chain):
         h = self.score_pool3_label(h)
         score_pool3_label = h  # 1/8
 
-        h = score_pool3_label[:, :,
-                              9:9 + upscore_pool4_label.data.shape[2],
-                              9:9 + upscore_pool4_label.data.shape[3]]
+        h = score_pool3_label[
+            :, :,
+            9:9 + upscore_pool4_label.data.shape[2],
+            9:9 + upscore_pool4_label.data.shape[3]
+        ]
         score_pool3c_label = h  # 1/8
 
         h = upscore_pool4_label + score_pool3c_label
@@ -158,14 +160,21 @@ class FCN8sMirrorDepthEstimation(chainer.Chain):
         h = self.upscore8_label(fuse_pool3_label)
         upscore8_label = h  # 1/1
 
-        h = upscore8_label[:, :,
-                           31:31 + rgb.shape[2], 31:31 + rgb.shape[3]]
+        h = upscore8_label[:, :, 31:31 + bgr.shape[2], 31:31 + bgr.shape[3]]
         score_label = h  # 1/1
 
-        return score_label, pool3, pool4, fc7
+        return score_label, pool3, pool4, pool5
 
-    def predict_depth(self, depth_viz, pool3, pool4, fc7):
-        h = self.score_fr_depth(fc7)
+    def predict_depth(self, depth_bgr, pool3, pool4, pool5):
+        h = F.relu(self.fc6_depth(pool5))
+        h = F.dropout(h, ratio=0.5)
+        fc6_depth = h  # 1/32
+
+        h = F.relu(self.fc7_depth(fc6_depth))
+        h = F.dropout(h, ratio=0.5)
+        fc7_depth = h  # 1/32
+
+        h = self.score_fr_depth(fc7_depth)
         score_fr_depth = h  # 1/32
 
         h = 0.01 * pool4
@@ -175,9 +184,11 @@ class FCN8sMirrorDepthEstimation(chainer.Chain):
         h = self.upscore2_depth(score_fr_depth)
         upscore2_depth = h  # 1/16
 
-        h = score_pool4_depth[:, :,
-                              5:5 + upscore2_depth.data.shape[2],
-                              5:5 + upscore2_depth.data.shape[3]]
+        h = score_pool4_depth[
+            :, :,
+            5:5 + upscore2_depth.data.shape[2],
+            5:5 + upscore2_depth.data.shape[3]
+        ]
         score_pool4c_depth = h  # 1/16
 
         h = upscore2_depth + score_pool4c_depth
@@ -190,9 +201,11 @@ class FCN8sMirrorDepthEstimation(chainer.Chain):
         h = self.score_pool3_depth(h)
         score_pool3_depth = h  # 1/8
 
-        h = score_pool3_depth[:, :,
-                              9:9 + upscore_pool4_depth.data.shape[2],
-                              9:9 + upscore_pool4_depth.data.shape[3]]
+        h = score_pool3_depth[
+            :, :,
+            9:9 + upscore_pool4_depth.data.shape[2],
+            9:9 + upscore_pool4_depth.data.shape[3]
+        ]
         score_pool3c_depth = h  # 1/8
 
         h = upscore_pool4_depth + score_pool3c_depth
@@ -201,15 +214,17 @@ class FCN8sMirrorDepthEstimation(chainer.Chain):
         h = self.upscore8_depth(fuse_pool3_depth)
         upscore8_depth = h  # 1/1
 
-        h = upscore8_depth[:, :,
-                           31:31 + depth_viz.shape[2],
-                           31:31 + depth_viz.shape[3]]
+        h = upscore8_depth[
+            :, :,
+            31:31 + depth_bgr.shape[2],
+            31:31 + depth_bgr.shape[3]
+        ]
         score_depth = h  # 1/1
 
         return score_depth
 
     def compute_loss_label(self, score_label, label_gt):
-        # segmentation loss
+        # Segmentation loss
         seg_loss = F.softmax_cross_entropy(
             score_label, label_gt, normalize=True)
 
@@ -218,10 +233,9 @@ class FCN8sMirrorDepthEstimation(chainer.Chain):
     def compute_loss_depth(self, score_depth, label_gt, depth_gt):
         assert label_gt.dtype == self.xp.int32
 
-        # regression loss
-        h = F.sigmoid(score_depth)  # (0, 1)
-        depth_pred = h * (self.max_depth - self.min_depth)
-        depth_pred += self.min_depth
+        # (-inf, inf) -> (0, 1) -> (min_depth, max_depth)
+        h = F.sigmoid(score_depth)
+        depth_pred = h * (self.max_depth - self.min_depth) + self.min_depth
 
         keep_regardless_mask = ~self.xp.isnan(depth_gt)
         if self.xp.sum(keep_regardless_mask) == 0:
@@ -240,6 +254,7 @@ class FCN8sMirrorDepthEstimation(chainer.Chain):
                 depth_pred[keep_only_mask],
                 depth_gt[keep_only_mask])
 
+        # Regression loss
         # XXX: What is proper loss function?
         coef = [1, 10]
         reg_loss = (coef[0] * depth_loss_regardless_mask +
@@ -247,118 +262,78 @@ class FCN8sMirrorDepthEstimation(chainer.Chain):
 
         return reg_loss
 
-    def compute_loss(self, score_label, score_depth,
-                     label_gt_fg, label_gt_bg, depth_gt_fg, depth_gt_bg):
-        score_label_fg = score_label[:, :self.n_class, :, :]
-        score_label_bg = score_label[:, self.n_class:, :, :]
-        # N, 2, H, W -> N, 1, H, W
-        score_depth_fg = score_depth[:, 0, :, :][:, self.xp.newaxis, :, :]
-        score_depth_bg = score_depth[:, 1, :, :][:, self.xp.newaxis, :, :]
+    def compute_loss(self, score_label, score_depth, label_gt, depth_gt):
+        seg_loss = self.compute_loss_label(score_label, label_gt)
+        reg_loss = self.compute_loss_depth(score_depth, depth_gt)
 
-        seg_loss_fg = self.compute_loss_label(score_label_fg, label_gt_fg)
-        seg_loss_bg = self.compute_loss_label(score_label_bg, label_gt_bg)
-        reg_loss_fg = self.compute_loss_depth(score_depth_fg, depth_gt_fg)
-        reg_loss_bg = self.compute_loss_depth(score_depth_bg, depth_gt_bg)
-
+        # Loss
         # XXX: What is proper loss function?
-        coef = [1, 1, 10, 10]
-        loss = (coef[0] * seg_loss_fg +
-                coef[1] * seg_loss_bg +
-                coef[2] * reg_loss_fg +
-                coef[3] * reg_loss_bg)
+        coef = [1, 10]
+        loss = coef[0] * seg_loss + coef[1] * reg_loss
         if self.xp.isnan(float(loss.data)):
             raise ValueError('Loss is nan.')
 
-        batch_size = len(score_label_fg)
+        batch_size = len(score_label)
         assert batch_size == 1
 
+        # GPU -> CPU
         # N, C, H, W -> C, H, W
-        label_gt_fg = cuda.to_cpu(label_gt_fg)[0]
-        label_gt_bg = cuda.to_cpu(label_gt_bg)[0]
-        label_pred_fg = cuda.to_cpu(F.argmax(score_label_fg, axis=1).data)[0]
-        label_pred_bg = cuda.to_cpu(F.argmax(score_label_bg, axis=1).data)[0]
-        depth_gt_fg = cuda.to_cpu(depth_gt_fg)[0]
-        depth_gt_bg = cuda.to_cpu(depth_gt_bg)[0]
-
-        h = F.sigmoid(score_depth)[:, 0, :, :]
-        depth_pred_fg = h * (self.max_depth - self.min_depth) + self.min_depth
-        depth_pred_fg = cuda.to_cpu(depth_pred_fg)[0]
-
-        h = F.sigmoid(score_depth)[:, 1, :, :]
-        depth_pred_bg = h * (self.max_depth - self.min_depth) + self.min_depth
-        depth_pred_bg = cuda.to_cpu(depth_pred_bg)[0]
+        label_gt = cuda.to_cpu(label_gt)[0]
+        label_pred = cuda.to_cpu(F.argmax(score_label, axis=1).data)[0]
+        depth_gt = cuda.to_cpu(depth_gt)[0]
+        h = F.sigmoid(score_depth)
+        depth_pred = h * (self.max_depth - self.min_depth) + self.min_depth
+        depth_pred = cuda.to_cpu(depth_pred)[0]
 
         # Evaluate Mean IU
-        mean_iu_fg = fcn.utils.label_accuracy_score(
-            [label_gt_fg], [label_pred_fg], n_class=self.n_class)[2]
-        mean_iu_bg = fcn.utils.label_accuracy_score(
-            [label_gt_bg], [label_pred_bg], n_class=self.n_class)[2]
+        miou = fcn.utils.label_accuracy_score(
+            [label_gt], [label_pred], n_class=self.n_class)[2]
 
         # Evaluate Depth Accuracy
-        depth_acc_fg = {}
-        depth_acc_bg = {}
-        for ground in ['fg', 'bg']:
-            if ground == 'fg':
-                label_gt = label_gt_fg
-                label_pred = label_pred_fg
-                depth_gt = depth_gt_fg
-                depth_pred = depth_pred_fg
+        depth_acc = {}
+        for thresh in [0.01, 0.03, 0.10, 0.30, 1.00]:
+            t_lbl_fg = label_gt > 0
+            p_lbl_fg = label_pred > 0
+            if np.sum(t_lbl_fg) == 0:
+                acc = np.nan
             else:
-                label_gt = label_gt_bg
-                label_pred = label_pred_bg
-                depth_gt = depth_gt_bg
-                depth_pred = depth_pred_bg
-            for thresh in [0.01, 0.02, 0.05]:
-                t_lbl_fg = label_gt > 0
-                p_lbl_fg = label_pred > 0
-                if np.sum(t_lbl_fg) == 0:
-                    acc = np.nan
-                else:
-                    depth_gt_cp = np.copy(depth_gt)
-                    depth_gt_cp[np.isnan(depth_gt_cp)] = np.inf
-                    numer = np.sum(np.logical_and(
+                # {TP and (|error| < thresh)} / (TP or FP or FN)
+                depth_gt_cp = np.copy(depth_gt)
+                depth_gt_cp[np.isnan(depth_gt_cp)] = np.inf
+                numer = np.sum(
+                    np.logical_and(
                         np.logical_and(t_lbl_fg, p_lbl_fg),
-                        np.abs(depth_gt_cp - depth_pred) < thresh))
-                    denom = np.sum(np.logical_or(t_lbl_fg, p_lbl_fg))
-                    acc = 1.0 * numer / denom
-                if ground == 'fg':
-                    depth_acc_fg['%.2f' % thresh] = acc
-                elif ground == 'bg':
-                    depth_acc_bg['%.2f' % thresh] = acc
+                        np.abs(depth_gt_cp - depth_pred) < thresh)
+                )
+                denom = np.sum(np.logical_or(t_lbl_fg, p_lbl_fg))
+                acc = 1.0 * numer / denom
+            depth_acc['%.2f' % thresh] = acc
 
         chainer.reporter.report({
             'loss': loss,
-            'seg_loss_fg': seg_loss_fg,
-            'seg_loss_bg': seg_loss_bg,
-            'reg_loss_fg': reg_loss_fg,
-            'reg_loss_bg': reg_loss_bg,
-            'mean_iu_fg': mean_iu_fg,
-            'mean_iu_bg': mean_iu_bg,
-            'depth_acc_fg<0.01': depth_acc_fg['0.01'],
-            'depth_acc_fg<0.02': depth_acc_fg['0.02'],
-            'depth_acc_fg<0.05': depth_acc_fg['0.05'],
-            'depth_acc_bg<0.01': depth_acc_bg['0.01'],
-            'depth_acc_bg<0.02': depth_acc_bg['0.02'],
-            'depth_acc_bg<0.05': depth_acc_bg['0.05'],
+            'seg_loss': seg_loss,
+            'reg_loss': reg_loss,
+            'miou': miou,
+            'depth_acc<0.01': depth_acc['0.01'],
+            'depth_acc<0.03': depth_acc['0.03'],
+            'depth_acc<0.10': depth_acc['0.10'],
+            'depth_acc<0.30': depth_acc['0.30'],
+            'depth_acc<1.00': depth_acc['1.00'],
         }, self)
 
         return loss
 
-    def __call__(self, rgb, depth_viz, label_gt_fg=None, label_gt_bg=None,
-                 depth_gt_fg=None, depth_gt_bg=None):
-        score_label, pool3, pool4, fc7 = self.predict_mask(rgb)
-        score_depth = self.predict_depth(depth_viz, pool3, pool4, fc7)
+    def __call__(self, bgr, depth_bgr, label_gt=None, depth_gt=None):
+        score_label, pool3, pool4, fc7 = self.predict_label(bgr, depth_bgr)
+        score_depth = self.predict_depth(depth_bgr, pool3, pool4, fc7)
         self.score_label = score_label
         self.score_depth = score_depth
 
-        if label_gt_fg is None or label_gt_bg is None or \
-           depth_gt_fg is None or depth_gt_bg is None:
+        if label_gt is None or depth_gt is None:
             assert not chainer.config.train
             return
 
-        loss = self.compute_loss(
-            score_label, score_depth,
-            label_gt_fg, label_gt_bg, depth_gt_fg, depth_gt_bg)
+        loss = self.compute_loss(score_label, score_depth, label_gt, depth_gt)
 
         return loss
 
@@ -375,8 +350,10 @@ class FCN8sMirrorDepthEstimation(chainer.Chain):
             if l.name == 'conv1_1':
                 l1 = getattr(vgg16, l.name)
                 l2 = getattr(self, l.name)
-                assert l1.W.size * 2 == l2.W.shape
-                assert l1.b.size == l2.b.size
+                assert l1.W.shape[0] == l2.W.shape[0]
+                assert l1.W.shape[1] * 2 == l2.W.shape[1]
+                assert l1.W.shape[2:] == l2.W.shape[2:]
+                assert l1.b.shape == l2.b.shape
                 l2.W.data[:, :l1.W.shape[1], :, :] = l1.W.data[...]
                 l2.W.data[:, l1.W.shape[1]:, :, :] = l1.W.data[...]
                 l2.b.data[...] = l1.b.data[...]
@@ -387,8 +364,8 @@ class FCN8sMirrorDepthEstimation(chainer.Chain):
                 assert l1.b.shape == l2.b.shape
                 l2.W.data[...] = l1.W.data[...]
                 l2.b.data[...] = l1.b.data[...]
-            elif l.name in ['fc6', 'fc7']:
-                l1 = getattr(vgg16, l.name)
+            elif l.name.startswith('fc'):
+                l1 = getattr(vgg16, l.name.split('_')[0])
                 l2 = getattr(self, l.name)
                 assert l1.W.size == l2.W.size
                 assert l1.b.size == l2.b.size
