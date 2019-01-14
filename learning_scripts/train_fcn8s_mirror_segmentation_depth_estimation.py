83912ea6a23719e9f8388abeffb63c28494d4252
Yuto Uchimi <momosama1072@gmail.com>
2019-01-14 16:20:30 +0900

    ExponentialShift of alpha: 0.99999 -> 0.99995

--- learning_scripts/train_fcn8s_mirror_segmentation_depth_estimation.py
+++ learning_scripts/train_fcn8s_mirror_segmentation_depth_estimation.py
@@ -148,7 +148,7 @@ def main():
 
     trainer = chainer.training.Trainer(updater, max_iter_epoch, out=out)
 
-    trainer.extend(extensions.ExponentialShift("alpha", 0.99999))
+    trainer.extend(extensions.ExponentialShift("alpha", 0.99995))
 
     if not osp.isdir(out):
         os.makedirs(out)
