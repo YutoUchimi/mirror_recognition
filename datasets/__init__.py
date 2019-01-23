3de3a914bf8004e1d3abe75b3129b75c2a945c72
Yuto Uchimi <momosama1072@gmail.com>
2019-01-23 20:42:07 +0900

    Add MultiViewMirror3DAnnotatedDataset class

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,3 +3,4 @@ from .labeled_mirror import LabeledMirrorDataset
 from .labeled_mirror import LabeledMirrorWithDepthDataset
 from .labeled_mirror_object import LabeledMirrorObjectDataset
 from .mirror_3d_annotated import Mirror3DAnnotatedDataset
+from .multi_view_mirror_3d_annotated import MultiViewMirror3DAnnotatedDataset
